780299e11238e8d39277c474d7bb065e7e64f590
Le Huy <lehuy@Les-MacBook-Air.local>
2015-02-25 15:02:17 +0700

    Initial Commit

---
+++ AppControllerClient/AppControllerClient.h
@@ -0,0 +1,13 @@
+//
+//  AppControllerClient.h
+//  AppControllerClient
+//
+//  Created by Le Huy on 2/25/15.
+//  Copyright (c) 2015 Huy Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppControllerClient : NSObject
+
+@end
---
+++ AppControllerClient/AppControllerClient.m
@@ -0,0 +1,13 @@
+//
+//  AppControllerClient.m
+//  AppControllerClient
+//
+//  Created by Le Huy on 2/25/15.
+//  Copyright (c) 2015 Huy Le. All rights reserved.
+//
+
+#import "AppControllerClient.h"
+
+@implementation AppControllerClient
+
+@end
